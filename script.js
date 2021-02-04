3182c2c7adb7580c1d5b399ac9a6afe144bb2284
a-developer-and-her-mac <64797765+a-developer-and-her-mac@users.noreply.github.com>
2021-02-04 14:29:07 -0800

    Feat/Style: update styling, make headers clickable, etc

--- script.js
+++ script.js
@@ -3,4 +3,5 @@ const toggleButton = document.querySelector('.toggle-button');
 const toggleMode = () => {
   const bodyElement  = document.body;
   bodyElement.classList.toggle('dark-mode');
+  toggleButton.classList.toggle('toggle-button-light');
 }
