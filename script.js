3376ba76365beab9e0c3392b5d16ab792b88bdf0
a-developer-and-her-mac <64797765+a-developer-and-her-mac@users.noreply.github.com>
2021-02-09 18:05:52 -0800

    Style/Refactor: restyle p tags, refactoring

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const toggleButton = document.querySelector('.toggle-button');
+const bodyElement  = document.body;
 
 const toggleMode = () => {
-  const bodyElement  = document.body;
   bodyElement.classList.toggle('dark-mode');
   toggleButton.classList.toggle('toggle-button-light');
 }
