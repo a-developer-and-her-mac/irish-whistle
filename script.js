d48f7d104e9d8c8e6fcf64b066f9117e3e4a2e05
a-developer-and-her-mac <64797765+a-developer-and-her-mac@users.noreply.github.com>
2021-03-27 16:17:06 -0700

    Feat: add mobile menu

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-const toggleButton = document.querySelector('.toggle-button');
-const bodyElement  = document.body;
+const toggleButton = document.querySelector('.toggle-button'),
+      bodyElement  = document.body,
+      hamburgerMenu = document.querySelector('.hamburger-menu'),
+      hamburger = document.getElementById("hamburger"),
+      navigationList = document.getElementById("navigation-list");
 
 const toggleMode = () => {
   bodyElement.classList.toggle('dark-mode');
   toggleButton.classList.toggle('toggle-button-light');
+  hamburgerMenu.classList.toggle('hamburger-menu-light');
 }
+
+hamburger.addEventListener("click", () => {
+  navigationList.classList.toggle("show");
+})
