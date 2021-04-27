17abfb2c63921db36c16830061464fb91471bf63
a-developer-and-her-mac <64797765+a-developer-and-her-mac@users.noreply.github.com>
2021-04-26 18:03:43 -0700

    Fix/Style: fix style bug with mobile menu in dark mode

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const toggleButton = document.querySelector('.toggle-button'),
       bodyElement  = document.body,
-      hamburgerMenu = document.querySelector('.hamburger-menu'),
+      hamburgerMenu = document.querySelectorAll('.hamburger-menu'),
       hamburger = document.getElementById("hamburger"),
       navigationList = document.getElementById("navigation-list");
 
 const toggleMode = () => {
   bodyElement.classList.toggle('dark-mode');
   toggleButton.classList.toggle('toggle-button-light');
-  hamburgerMenu.classList.toggle('hamburger-menu-light');
+
+  for (let i = 0; i < hamburgerMenu.length; i++) {
+    hamburgerMenu[i].classList.toggle('hamburger-menu-light');
+  };
 }
 
 hamburger.addEventListener("click", () => {
